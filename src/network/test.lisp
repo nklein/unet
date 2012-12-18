cbf22ca8c3f0faccc33c237c16218d1695549c0c
Patrick Stein <pat@nklein.com>
2012-12-17 21:54:58 -0600

    Started adding threaded test

--- src/network/test.lisp
+++ src/network/test.lisp
@@ -83,3 +83,76 @@
       (send-datagram alice "Hello" bob)
       (close-socket bob)
       (recv-datagram bob))))
+
+;;; Make sure we can prepare a thread-safe mock-network-provider
+#+thread-support
+(nst:def-fixtures mock-mt-network-provider
+    (:documentation "Defines a thread-safe mock network provider called PROVIDER")
+  (provider (make-instance 'unet-network-mock:network-provider
+                           :mutex (bordeaux-threads:make-lock))))
+
+#+thread-support
+(nst:def-test-group mock-mt-make-provider-tests (mock-mt-network-provider)
+  (nst:def-test mock-make-provider (:true)
+    provider))
+
+(defun run-send-recv (&key local remote (to-send 0) (to-recv 0))
+  (let ((counter 0))
+    (labels ((run (to-send to-recv)
+               #+thread-support
+               (bordeaux-threads:thread-yield)
+               (flet ((send ()
+                        (let ((msg (format nil "Hi ~A" (incf counter))))
+                          (send-datagram local msg remote))
+                        (run (1- to-send) to-recv))
+                      
+                      (recv ()
+                        (multiple-value-bind (msg from) (recv-datagram local)
+                          (cond
+                            (from
+                            
+                             (format t "~A GOT: ~A from ~A~%"
+                                     (incf counter) msg from)
+                             (run to-send (1- to-recv)))
+                            (t (run to-send to-recv)))))
+                      
+                      (cont ()
+                        (incf counter)
+                        (run to-send to-recv)))
+                 
+                 (case (mod counter 3)
+                   (0 (cond
+                        ((plusp to-send) (send))
+                        ((plusp to-recv) (recv))))
+                   (1 (cond
+                        ((plusp to-send) (send))
+                        (t (cont))))
+                   (2 (cond
+                        ((plusp to-recv) (recv))
+                        (t (cont))))))))
+
+      (lambda () (run to-send to-recv)))))
+
+(defun run-send-recv-thread (&key local remote (to-send 0) (to-recv 0))
+  (bordeaux-threads:make-thread (run-send-recv :local local
+                                               :remote remote
+                                               :to-send to-send
+                                               :to-recv to-recv)))
+
+(defun wait-for-message (local)
+  #+thread-support
+  (bordeaux-threads:thread-yield)
+  (multiple-value-bind (msg from) (recv-datagram local)
+    (if from
+        (values msg from)
+        (wait-for-message local))))
+
+#+thread-support
+(nst:def-test-group mock-mt-socket-tests (mock-mt-network-provider
+                                          alice-and-bob)
+  (nst:def-test mock-mt-send-one-message (:values (:equal "Hi 1")
+                                               (:equal alice))
+    (let ((thread (run-send-recv-thread :local alice :remote bob :to-send 1)))
+      (unwind-protect
+          (wait-for-message bob)
+        (bordeaux-threads:join-thread thread)))))
