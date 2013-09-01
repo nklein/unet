adb2d3c5fd684a42778be83a832fe1568cd01aad
Patrick Stein <pat@nklein.com>
2013-09-01 11:05:50 -0500

    Added asdf:test-op

--- src/logging/test.lisp
+++ src/logging/test.lisp
@@ -1,6 +1,6 @@
 (defpackage :unet-logging-test
   (:use :cl)
-  
+
   (:import-from :unet-logging
                 :make-logger
                 :add-logger-category
@@ -14,7 +14,7 @@
 (nst:def-test-group existence-tests ()
   (nst:def-test logging-package-exists (:true)
     (find-package :unet-logging))
-  
+
   (nst:def-test logging-functions-exist (:each (:function-ish))
     '(make-logger
       add-logger-category
@@ -30,13 +30,13 @@
 (nst:def-test-group instantiate-logger (simple-logger)
   (nst:def-test make-logger-succeeds (:true)
     logger)
-  
+
   (nst:def-test add-base-category-succeeds (:eq :animal)
     (add-logger-category logger :animal))
 
   (nst:def-test add-sub-category-succeeds (:eq :animal)
     (add-logger-category logger :animal :mammal))
-  
+
   (nst:def-test add-multiple-sub-categories-succeeds (:eq :mammal)
     (add-logger-category logger :mammal :dog :cat :mouse :human)))
 
@@ -52,29 +52,34 @@
 
 (defmacro with-logging-to-string ((logger &rest categories) &body body)
   (let ((handle (gensym "LOGGER-HANDLE-"))
-        (stream (gensym "STREAM-")))
-    `(with-output-to-string (,stream)
-       (start-logging ,logger ,stream ',handle ,@categories)
-       (unwind-protect
-            (progn ,@body)
-         (stop-logging ,logger ',handle)))))
+        (stream (gensym "STREAM-"))
+        (string (gensym "STRING-")))
+    `(let ((,string (make-array '(0)
+                                :element-type 'base-char
+                                :fill-pointer 0
+                                :adjustable t)))
+       (with-output-to-string (,stream ,string)
+         (start-logging ,logger ,stream ',handle ,@categories)
+         (unwind-protect
+              (progn ,@body)
+           (stop-logging ,logger ',handle))))))
 
 (nst:def-test-group log-simple-messages (simple-logger categoried-logger)
   (nst:def-test log-a-message-to-nowhere (:not :err)
     (log-string logger :dog "Woof"))
-  
+
   (nst:def-test log-message-works (:regex ".*DOG Woof$")
     (with-logging-to-string (logger)
       (log-string logger :dog "Woof")))
-  
+
   (nst:def-test log-message-for-subcategory (:regex ".*DOG Woof$")
     (with-logging-to-string (logger :mammal)
       (log-string logger :dog "Woof")))
-  
+
   (nst:def-test log-message-unclaimed (:equal "")
     (with-logging-to-string (logger :mammal)
       (log-string logger :fish "Bloop")))
-  
+
   (nst:def-test log-messages-to-different-logs
       (:values (:regex ".*CAT Meow.*DOG Woof$") (:regex ".*FISH Bloop$"))
     (let (m f)
--- unet.asd
+++ unet.asd
@@ -41,3 +41,27 @@
                      ;;; other system files
                      (:static-file "README.mkdn")
                      (:static-file "LICENSE.txt")))))
+
+(asdf:defsystem :unet-test
+  :description
+    "unet-test: test suite for unet"
+  :version "0.1.2012.11.29"
+  :author "Patrick Stein <pat@nklein.com>"
+  :encoding :utf-8
+  :licence "MIT"
+  :depends-on ("unet" "nst" "cl-ppcre")
+  :components
+    ((:module "src"
+        :components (;;; logging function test
+                     (:file "test/base")
+                     (:file "logging/test"
+                        :depends-on ("test/base"))
+                     (:file "network/test"
+                        :depends-on ("test/base"))
+                     (:file "test/run")
+                     (:static-file "LICENSE.txt")))))
+
+(defmethod asdf:perform ((op asdf:test-op)
+                         (system (eql (asdf:find-system :unet))))
+  (asdf:load-system :unet-test)
+  (funcall (find-symbol (symbol-name :run-tests) :unet-test)))
